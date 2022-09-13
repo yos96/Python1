a9c59fe6d88d32a674ac4c7d85aaa37521ba94db
Maria Yocelin Recoba Ascencion <yos9626@gmail.com>
2022-09-13 10:32:23 -0500

    primer commit

---
+++ hola.py
@@ -0,0 +1,5 @@
+name = input("Ingrea tu nombre: ")
+age = input("Ingresa tu edad: ")
+print(f"Yo soy {name} y tengo {age} años")
+future = int(age) + 4
+print(f"En el año 2026 tendre {future} años")
